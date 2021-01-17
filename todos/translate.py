0128dde362b9a36da1b9d5c0230ff47ac28ca3ec
EC2 Default User <ec2-user@ip-172-31-3-224.ec2.internal>
2021-01-17 08:39:11 +0000

    second commit for translate function

--- todos/translate.py
+++ todos/translate.py
@@ -4,6 +4,7 @@ import json
 from todos import decimalencoder
 import boto3
 dynamodb = boto3.resource('dynamodb')
+trans = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True);
 
 
 def translate(event, context):
@@ -15,11 +16,20 @@ def translate(event, context):
             'id': event['pathParameters']['id']
         }
     )
+    
+    xjson = json.dumps(result['Item'],
+                           cls=decimalencoder.DecimalEncoder)
+                           
+    xpython = json.loads(xjson)
+    
+    xjsonResult = xpython.dumps(xpython)
+    
+    xresult = trans.translate_text(Text=xpython["text"], SourceLanguageCode="en", TargetLanguageCode="de")
 
     # create a response
     response = {
         "statusCode": 200,
-        "body": json.dumps(result['Item'],
+        "body": json.dumps(xjsonResult,
                            cls=decimalencoder.DecimalEncoder)
     }
 
